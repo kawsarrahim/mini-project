597d83b7b2d6cde2e22792d44acd140159e9267b
kawsarrahim <87430648+kawsarrahim@users.noreply.github.com>
2021-07-25 22:39:59 +0100

    First commit

---
+++ Wk1MiniProject.py
@@ -0,0 +1,22 @@
+products_list = ["Water", "Coke", "Pepsi", "Mango", "Sprite"]
+
+
+app_option = int(input(''' Input your option
+                    Press 0 -> To Exit App 
+                    Press 1 -> To Enter App
+                    Enter your option here: '''))
+if app_option == 0:
+    print("Goodbye.")
+    exit()
+
+elif app_option == 1:
+    user_input = int(input(''' Your menu options below:
+    0: Main Menu
+    1: View products menu
+    2: Add a new product 
+    3: Update products menu
+    4: Delete a product
+    \tEnter Option Here: '''))
+
+if user_input == 0:
+    
